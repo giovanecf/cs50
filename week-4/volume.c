908897501c043f43cfdc6351b1a1cbebfa5483b0
Geovani Figueiredo <geovani@SeuMadruga.vila.figueiredo.io>
2024-09-14 12:55:37 -0300

    done volume.c

--- week-4/volume.c
+++ week-4/volume.c
@@ -8,6 +8,7 @@
 const int HEADER_SIZE = 44;
 
 typedef uint8_t BYTE;
+typedef uint16_t DOUBLE_BYTE;
 
 int main(int argc, char *argv[])
 {
@@ -36,17 +37,91 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    BYTE *byte_c[HEADER_SIZE];
-    for (int i = 0; i < HEADER_SIZE; i++)
-    {   
-        fread(byte_c[i], sizeof(BYTE), 1, input);
-        printf("%d", byte_c[i]);
-    }
+    BYTE header_buffer[HEADER_SIZE];
+    fread(&header_buffer, 44, 1, input);
+    //printf("%d\n", header_buffer);
+    fwrite(&header_buffer, 44, 1, output);
 
 
     // TODO: Read samples from input file and write updated data to output file
+    DOUBLE_BYTE sample_buffer = 0;
+    while(fread(&sample_buffer, sizeof(sample_buffer), 1, input) != 0)
+    {
+        DOUBLE_BYTE amp_sample_buffer = sample_buffer * factor;
+        //printf("%f, %d, %d\n", factor, sample_buffer, amp_sample_buffer);
+        fwrite(&amp_sample_buffer, sizeof(sample_buffer), 1, output);
+    }
 
     // Close files
     fclose(input);
     fclose(output);
 }
+
+/**
+      >>> BUT, what is the transfer speed?
+    
+      >>>>> MACHINE
+    
+           _,met$$$$$gg.          geovani@SeuMadruga 
+        ,g$$$$$$$$$$$$$$$P.       ------------------ 
+      ,g$$P"     """Y$$.".        OS: Debian GNU/Linux 12 (bookworm) x86_64 
+     ,$$P'              `$$$.     Host: 270E5K/270E5Q/271E5K/2570EK P08RDH 
+    ',$$P       ,ggs.     `$$b:   Kernel: 6.1.0-25-amd64 
+    `d$$'     ,$P"'   .    $$$    Uptime: 7 days, 2 hours, 56 mins 
+     $$P      d$'     ,    $$P    Packages: 1853 (dpkg), 4 (snap) 
+     $$:      $$.   -    ,d$$'    Shell: bash 5.2.15 
+     $$;      Y$b._   _,d$P'      Resolution: 1920x1080 
+     Y$$.    `.`"Y$$$$P"'         DE: GNOME 43.9 
+     `$$b      "-.__              WM: Mutter 
+      `Y$$                        WM Theme: Adwaita 
+       `Y$$.                      Theme: Adwaita [GTK2/3] 
+         `$$b.                    Icons: Adwaita [GTK2/3] 
+           `Y$$b.                 Terminal: gnome-terminal 
+              `"Y$b._             CPU: Intel i5-5200U (4) @ 2.100GHz 
+                  `"""            GPU: Intel HD Graphics 5500 
+                                  GPU: NVIDIA GeForce 920M 
+                                  Memory: 6408MiB / 7836MiB
+                                  SSD: 120GB
+
+
+    >>>>> DATA
+
+    geovani@SeuMadruga:~/app/cs50/week-4$ time ./volume input.wav output.wav 0.1
+    real    0m0,021s <----- TIME
+    user    0m0,021s
+    sys     0m0,000s
+    geovani@SeuMadruga:~/app/cs50/week-4$ ls -la
+    total 724
+    drwxr-xr-x  2 geovani geovani   4096 set 14 12:43 .
+    drwxr-xr-x 10 geovani geovani   4096 set 12 22:42 ..
+    -rw-r--r--  1 geovani geovani 352844 dez 31  2021 input.wav
+    -rw-r--r--  1 geovani geovani 352844 set 14 12:43 output.wav <---- SIZE
+    -rwxr-xr-x  1 geovani geovani  16240 set 14 12:43 volume
+    -rw-r--r--  1 geovani geovani   1442 set 14 12:43 volume.c
+
+    >>>>> CALC
+
+    speed = SIZE / TIME
+            352844 B / 0.021s
+            0.352844 MB / 0.021s
+
+    speed = 16.802095238 MB/s
+
+    
+    >>> COMPARE TO REGULAR 'cp'
+
+    geovani@SeuMadruga:~/app/cs50/week-4$ time cp input.wav output.wav
+
+    real    0m0,015s
+    user    0m0,011s
+    sys     0m0,001s
+
+    >>>>> CALC
+
+    speed = SIZE / TIME
+            352844 B / 0.015s
+            0.352844 MB / 0.015s
+
+    speed = 23.522933333 MB/s
+
+*/
